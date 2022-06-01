e074b4ec927b4908afdfe04419c9c885507139c2
khaled39ab <khaled39ab@gmail.com>
2022-06-01 23:46:44 +0600

    create input email password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { getAuth } from "firebase/auth";
 import './App.css';
+import app from './firebase.init';
 
+
+const auth = getAuth(app);
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <form>
+            <>Email:</>
+            <input type="email" name="" id="" /> <br />
+            <>Password:</>
+            <input type="password" name="" id="" />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEXLt5WyqbKzhg5wmz0nMGHcbyTjDTsyg",
+  authDomain: "email-password-auth-e2632.firebaseapp.com",
+  projectId: "email-password-auth-e2632",
+  storageBucket: "email-password-auth-e2632.appspot.com",
+  messagingSenderId: "716687436555",
+  appId: "1:716687436555:web:dc5fd77a26b6de2938d00e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
