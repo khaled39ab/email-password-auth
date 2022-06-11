d2a83718eecb38f9b4491d67ce0af36ef1d9a1be
khaled39ab <khaled39ab@gmail.com>
2022-06-11 10:47:33 +0600

    sent reset email

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap';
 import './LogIn.css'
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import app from '../../firebase.init';
 
@@ -12,7 +12,8 @@ const LogIn = () => {
     const [validated, setValidated] = useState(false);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('')
+    const [error, setError] = useState('');
+    const [submitted, setSubmitted] = useState('')
 
     const handleEmail = e => {
         setEmail(e.target.value)
@@ -42,6 +43,23 @@ const LogIn = () => {
             })
     }
 
+    const passwordResetEmail = e =>{
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+            setValidated(true);
+            return;
+        }
+
+        sendPasswordResetEmail(auth, email)
+        .then(() =>{
+            setSubmitted('Password reset email sent')
+        })
+        .catch(err =>{
+            setError(err.message)
+            console.log(err);
+        })
+    }
     return (
         <div className='logIn-container p-4 m-4 w-50 mx-auto'>
             <h2 className='text-center text-info'>Please Log In</h2>
@@ -57,18 +75,19 @@ const LogIn = () => {
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required />
-                    <Form.Text className="text-muted">
-                        Password should minimum 6 character with 1 special character.
-                    </Form.Text>
                     <Form.Control.Feedback type="invalid">
                         Please provide a valid password.
                     </Form.Control.Feedback>
                 </Form.Group>
-                <p className="text-danger">{error}</p>
+
+                {
+                    error ? <p className="text-danger">{error}</p> : <p className='text-success'>{submitted}</p>
+                }
+
                 <Button variant="primary" type="submit">
                     Log in
                 </Button>
-                <Button variant="link">Forget Password?</Button>
+                <Button onClick={passwordResetEmail} variant="link">Forget Password?</Button>
                 <hr />
             </Form>
             <div className='create-btn-sec'>
